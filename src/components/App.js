4048c96b62ec132691857ffbdeae5429f7e0c1c8
Richard-JS <Richardcontato1997@gmail.com>
2020-09-04 11:49:42 -0300

    git push

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Welcome from '../components/welcome';
+
+export default class App extends Component {
+  render() {
+    return (
+    <Welcome
+    titulo = 'Canal Geek Dev'
+    slogan = 'Cuurso'
+    />
+  )
+} 
+};
---
+++ src/styles/index.js
@@ -0,0 +1,21 @@
+import{StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#0066cc',
+      alignItems: 'center',
+      justifyContent: 'center',
+     
+    },
+    text:{
+      color:'#fff',
+      fontSize:20
+    },
+    slogan:{
+      color:'#000'
+      
+    }
+  });
+  
+export default styles;
